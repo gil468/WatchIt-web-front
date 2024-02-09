d717ae3b13b969e9813b6d1bb9b7ba775248ee90
Oren <oren.hta@gmail.com>
2024-02-09 12:13:35 +0200

    optional author

--- src/components/UserAndTimestampCardHeader.tsx
+++ src/components/UserAndTimestampCardHeader.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { format } from "date-fns";
 
 interface UserAndTimestampCardHeaderProps {
-  author: { fullName: string; imgUrl: string };
+  author: { fullName?: string; imgUrl?: string };
   timeStamp: Date;
 }
 
